635a498984a04434f0c0d5019f2943028d229269
chadaway-cc <rebeccahadaway@gmail.com>
2012-08-31 10:24:49 -0400

    final exam and I am stuck

---
+++ Final Exam/model/db.sql
@@ -0,0 +1,60 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.2
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Aug 31, 2012 at 04:13 PM
+-- Server version: 5.5.25a
+-- PHP Version: 5.4.4
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `jobhunt`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jobhunt`
+--
+
+CREATE TABLE IF NOT EXISTS `jobhunt` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `pubDate` date NOT NULL,
+  `title` varchar(25) NOT NULL,
+  `description` varchar(100) NOT NULL,
+  `link` varchar(35) NOT NULL,
+  `author` int(25) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jobhunt3`
+--
+
+CREATE TABLE IF NOT EXISTS `jobhunt3` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `datep` date NOT NULL,
+  `title` varchar(25) NOT NULL,
+  `description` varchar(100) NOT NULL,
+  `link` varchar(50) NOT NULL,
+  `postedby` varchar(25) NOT NULL,
+  `nextaction` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+
